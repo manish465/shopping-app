1de3d51a6744c9ff4e490c6a718c69616dc708dd
manish <ms5978307@gmail.com>
2023-07-19 21:30:21 +0530

    added some features
    - added gateway
    - added service discovery
    - added load balancer

--- order-service/src/main/java/com/manish/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/manish/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodeList).build())
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://InventoryServiceMS/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodeList).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
